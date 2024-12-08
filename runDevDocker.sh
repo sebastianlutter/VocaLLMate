d5a8e792213304daef05cd9de78e268033dfe429
Sebastian Lutter <sebastian.lutter@brettingham.de>
2024-12-08 21:44:02 +0100

    Mount huggingface and nltk_data folders from host system so downloads are only done once

--- runDevDocker.sh
+++ runDevDocker.sh
@@ -1,11 +1,18 @@
-#!/usr/bin
+#!/bin/bash
 #
 # Run to get a dev docker with torch environment using the
 # Dockerfile in the repository so the transformer TTS can use GPU hardware
 #
+
+set -e
+
 IMAGE_NAME="local-servant-llm:latest"
 USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 
 docker build --build-arg USER_ID=${USER_ID} --build-arg GROUP_ID=${GROUP_ID} -t ${IMAGE_NAME} .
-docker run --rm -it --gpus all -v "$(pwd)":/app ${IMAGE_NAME} bash
+docker run --rm -it --gpus all \
+           -v ~/.cache/huggingface/hub:/home/servant/.cache/huggingface/hub \
+           -v ~/nltk_data/:/home/servant/nltk_data/ \
+           --privileged \
+           -v "$(pwd)":/app ${IMAGE_NAME} bash
